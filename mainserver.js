be82a0d60937127ced50af6c6fae765b2ceb621d
sai70977venu <saivenu121314@gmail.com>
2020-07-17 01:34:44 +0530

    Initial Commit

--- mainserver.js
+++ mainserver.js
@@ -7,7 +7,7 @@ var bodyParser = require('body-parser');
 const auth = require("./routes/auth");    //authentication router
 var app = express();
 var logger =require("./config/logger")
-// require('dotenv').config();
+require('dotenv').config();
 app.use(bodyParser.json());
 
 var urlencodedparser=bodyParser.urlencoded({extended:false})
@@ -15,7 +15,7 @@ app.use("/users/",urlencodedparser,auth);
 
 //running on the port
 let PORT = process.env.PORT || process.env.PUBLIC_PORT;
-app.listen(process.env.PUBLIC_PORT,() => {
+app.listen(PORT,() => {
     console.log("server connected", process.env.PUBLIC_PORT);
 })
 //Set up default mongoose connection
